6295cb5aa92a276fde3e0d8a80f4d8fe0a0f072a
Jose Vega <jevf@gft.com>
2023-03-31 14:21:58 +0200

    styling
    Signed-off-by: Jose Vega <jevf@gft.com>

--- sect4/src/components/Expenses/Expenses.js
+++ sect4/src/components/Expenses/Expenses.js
@@ -15,17 +15,21 @@ const Expenses = ({items}) => {
   }
   
   return (
+    <>
     <Card className="expenses">
-      <ExpensesFilter selectHandler={selectHandler} selectedYear={selectedYear}/>
+      <ExpensesFilter selectedYear={selectedYear} selectHandler={selectHandler}/>
+    </Card>
+    <Card className="expenses">
       {filteredItems.map(item => (
         <ExpenseItem
-          key={item.id}
-          title={item.title}
-          amount={item.amount}
-          date={item.date}
+        key={item.id}
+        title={item.title}
+        amount={item.amount}
+        date={item.date}
         />
-      ))}
+        ))}
     </Card>
+        </>
   );
 }
 
